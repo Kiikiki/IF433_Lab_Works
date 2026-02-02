dc79292841a5ef390f7a81efdf2642cc92faea59
Kiikiki <allysiakiara@gmail.com>
2026-02-02 16:23:58 +0700

    Week01: Refactor to use val and string templates

--- src/oop_110866_Kiara/week01/GradingSystem.kt
+++ src/oop_110866_Kiara/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_110866_Kiara.week01
 
 fun main () {
-    var name: String = "John Doe"
-    var score: Int = 0
+    val name = "John Doe"
+    val score = 0
 
-    println("Name: " + name + ", Score: " + score)
+    println("Name: $name , Score: $score")
 }
