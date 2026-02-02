418947cf374c8502113029a94606572c765ff23b
Kiikiki <allysiakiara@gmail.com>
2026-02-02 23:23:47 +0700

    Week01: (task 2) implement discount logic

--- src/oop_110866_Kiara/week01/GameStore.kt
+++ src/oop_110866_Kiara/week01/GameStore.kt
@@ -1,6 +1,18 @@
 package oop_110866_Kiara.week01
 
 fun main() {
+    //note not sure if input should be added so default input
     val gameTitle = "Minecraft"
     val price = 320000
+
+    val finalPrice = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int): Int {
+    if(price > 500000) {
+        return price * 0,8 //20% discount
+    }
+    else {
+        return price * 0,9 //10% discount
+    }
 }
