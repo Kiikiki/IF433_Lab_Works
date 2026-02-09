2c1f325212257879235a35a20564337e12455487
Kiikiki <allysiakiara@gmail.com>
2026-02-09 22:07:13 +0700

    Week02: implement selection logc for constructors

--- src/oop_110866_Kiara/week02/Main.kt
+++ src/oop_110866_Kiara/week02/Main.kt
@@ -25,4 +25,22 @@ fun Main() {
         val s1 = Student(name, nim, major)
         println("Status: Registration successful!")
     }
+
+    print("Pick a choice (1. Regular, 2. Public): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if(type == 1) {
+        print("Enter major: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Registered at: ${s1.major} with an initial GPA of ${s1.gpa}")
+    }
+    else if(type == 2) {
+        val s2 = Student(name, nim)
+        println("Registered at: ${s2.major} with an initial GPA of ${s2.gpa}")
+    }
+    else {
+        println("Choice not available, registration failed!")
+    }
 }
