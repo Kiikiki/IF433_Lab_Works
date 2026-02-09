4342607d9dcfeb41a7afbe8fb8fda973be5ab0c9
Kiikiki <allysiakiara@gmail.com>
2026-02-09 16:56:27 +0700

    Week02: add warning validation in init block

--- src/oop_110866_Kiara/week02/Student.kt
+++ src/oop_110866_Kiara/week02/Student.kt
@@ -1,4 +1,13 @@
 package oop_110866_Kiara.week02
 
 class Student (val name: String, val nim: String, var major: String ) {
+    init {
+        if (nim.length != 5) {
+            println("WARNING: object is made with nim ($nim) that isn't valid!")
+            println("Student data $name might be problematic in the system.")
+        }
+        else {
+            println("LOG: object student $name is successfully located in the memory!")
+        }
+    }
 }
