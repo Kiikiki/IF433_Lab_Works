a4e23831329569b5d1c7710618685d4ef6955f87
Kiikiki <allysiakiara@gmail.com>
2026-02-16 15:08:58 +0700

    week03: fix setter using backing field

--- src/oop_110866_Kiara/week03/Employee.kt
+++ src/oop_110866_Kiara/week03/Employee.kt
@@ -3,9 +3,19 @@ package oop_110866_Kiara.week03
 class Employee(val name: String) {
     var salary: Int = 0
 
-    //infinite loop
+    /*infinite loop
+     set(value) {
+         println("Salary: $value")
+         this.salary = value
+     } */
+
     set(value) {
-        println("Salary: $value")
-        this.salary = value
+        if(value < 0) {
+            println("ERROR: Wage cannot be negative. Will automatically be set to 0!")
+            field = 0;
+        }
+        else {
+            field = value
+        }
     }
 }
