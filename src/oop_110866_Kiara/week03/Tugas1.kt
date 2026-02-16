76bc410a83fa664133a635676442291f7819b59e
Kiikiki <allysiakiara@gmail.com>
2026-02-16 16:13:12 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_110866_Kiara/week03/Tugas1.kt
+++ src/oop_110866_Kiara/week03/Tugas1.kt
@@ -2,18 +2,13 @@ package oop_110866_Kiara.week03
 
 fun main() {
     println("=== Your Weapon ===")
-    //testing Epic tier
-    val item1 = Weapon("The Slightly Sharp Object")
-    item1.damage = 650
-    println("Name: ${item1.name}, Damage: ${item1.damage}, Tier: ${item1.tier}")
+    val item = Weapon("Sir Stabs-a-Lot")
 
-    //testing overpowered weapon
-    val item2 = Weapon("Regret")
-    item2.damage = 9999
-    println("Name: ${item2.name}, Damage: ${item2.damage}, Tier: ${item2.tier}")
+    item.damage = -50
+    println("Name: ${item.name}, Damage: ${item.damage}, Tier: ${item.tier}")
 
-    //testing insanely bad weapon
-    val item3 = Weapon("Excalipoor")
-    item3.damage = -50
-    println("Name: ${item3.name}, Damage: ${item3.damage}, Tier: ${item3.tier}")
+    item.damage = 9999
+    println("Name: ${item.name}, Damage: ${item.damage}", "Tier: ${item.tier}")
+
+
 }
