a905d599fac21091f77c9b0bf9aef5de18f1bde3
Kiikiki <allysiakiara@gmail.com>
2026-02-16 15:23:59 +0700

    week03: cleanup code

--- src/oop_110866_Kiara/week03/Employee.kt
+++ src/oop_110866_Kiara/week03/Employee.kt
@@ -3,12 +3,6 @@ package oop_110866_Kiara.week03
 class Employee(val name: String) {
     var salary: Int = 0
 
-    /*infinite loop
-     set(value) {
-         println("Salary: $value")
-         this.salary = value
-     } */
-
     set(value) {
         if(value < 0) {
             println("ERROR: Wage cannot be negative. Will automatically be set to 0!")
--- src/oop_110866_Kiara/week03/Main.kt
+++ src/oop_110866_Kiara/week03/Main.kt
@@ -3,14 +3,11 @@ package oop_110866_Kiara.week03
 fun main() {
     val e = Employee("Budi")
 
-    //Salary test
     e.salary = -1000
     e.salary = 5000000
     println("Wage: ${e.salary}")
 
-    //Encapsulation test
     e.increasePerformance()
 
-    //Computation test (tax)
     println("Tax: ${e.tax}")
 }
