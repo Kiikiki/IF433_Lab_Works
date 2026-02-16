1f76cfdace55129114d18f3095e355d962eaf605
Kiikiki <allysiakiara@gmail.com>
2026-02-16 14:59:39 +0700

    week03: simulate stack overflow error in setter

--- src/oop_110866_Kiara/week03/Employee.kt
+++ src/oop_110866_Kiara/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_110866_Kiara.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+
+    //infinite loop
+    set(value) {
+        println("Salary: $value")
+        this.salary = value
+    }
 }
