0bee1f4ba77c05bc8373baec1d956a857cf9d44d
Kiikiki <allysiakiara@gmail.com>
2026-02-16 15:23:26 +0700

    week03: test all encapsulation logic in main

--- src/oop_110866_Kiara/week03/Main.kt
+++ src/oop_110866_Kiara/week03/Main.kt
@@ -2,10 +2,15 @@ package oop_110866_Kiara.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
 
-    e.printStatus()
+    //Salary test
+    e.salary = -1000
+    e.salary = 5000000
+    println("Wage: ${e.salary}")
+
+    //Encapsulation test
     e.increasePerformance()
-    e.printStatus()
+
+    //Computation test (tax)
+    println("Tax: ${e.tax}")
 }
