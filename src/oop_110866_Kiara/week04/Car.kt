6c49c48d85c600f2a1f9beff7fb2d737058151d7
Kiikiki <allysiakiara@gmail.com>
2026-02-23 13:26:41 +0700

    week04: override honk method in car

--- src/oop_110866_Kiara/week04/Car.kt
+++ src/oop_110866_Kiara/week04/Car.kt
@@ -1,4 +1,12 @@
 package oop_110866_Kiara.week04
 
-class Car {
+class Car(brand: String, val numberOfDoors: Int): Vehicle(brand) {
+    fun openTrunk() {
+        println("Car opening trunk with $numberOfDoors doors.")
+    }
+
+    //modifictaion
+    override fun honk() {
+        println("Honk! HONK woi AWAS WOI! $brand lewat!")
+    }
 }
