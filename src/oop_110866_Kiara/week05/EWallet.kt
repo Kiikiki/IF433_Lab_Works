4cabedc054a22ddc53b637111eb1a0d85a85194b
Kiikiki <allysiakiara@gmail.com>
2026-03-02 15:16:21 +0700

    week05: (tugas 2) create EWallet class

---
+++ src/oop_110866_Kiara/week05/EWallet.kt
@@ -0,0 +1,19 @@
+package oop_110866_Kiara.week05
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance < amount) {
+            println("[payment process] A fly comes out of your balance. They don't accept flies")
+        }
+        else {
+            balance -= amount
+            println("[payment process] Your balance is now $balance! I can hear mr crabs crying")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("[top up] You have inserted $amount")
+        println("The sound of cash fills your balance to $balance")
+    }
+}
