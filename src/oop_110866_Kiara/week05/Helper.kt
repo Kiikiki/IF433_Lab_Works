33d09dd695e29c4a5aa466fef195d1658038f6bd
Kiikiki <allysiakiara@gmail.com>
2026-03-02 15:04:34 +0700

    week05: (tugas 1) test MathHelper overloading in main

--- src/oop_110866_Kiara/week05/Helper.kt
+++ src/oop_110866_Kiara/week05/Helper.kt
@@ -1,5 +1,11 @@
 package oop_110866_Kiara.week05
 
 fun main() {
+    val math = MathHelper()
 
+    val square = math.calculateArea(4)
+    val rectangle = math.calculateArea(4, 3)
+    val circle = math.calculateArea(2.1)
+
+    println("square: $square, rectangle: $rectangle, circle: $circle")
 }
