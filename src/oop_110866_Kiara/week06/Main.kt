957da3086b49495718f1640b534f7e69b58a7c76
Kiikiki <allysiakiara@gmail.com>
2026-03-09 13:04:54 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/oop_110866_Kiara/week06/Main.kt
@@ -0,0 +1,21 @@
+package oop_110866_Kiara.week06
+
+fun processCheckout(method: PaymentMethod, amount: Double) {
+    println("-> Starting checkout...")
+    method.pay(amount)
+}
+
+fun main() {
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== Testing Checkout ===")
+    processCheckout(pay1, 50000.0)
+    processCheckout(pay2, 150000.0)
+}
