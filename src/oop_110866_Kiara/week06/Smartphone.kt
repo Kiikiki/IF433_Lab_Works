8bdda47a4db65e50e45a5b1636e493bb0f834760
Kiikiki <allysiakiara@gmail.com>
2026-03-09 12:39:25 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_110866_Kiara/week06/Smartphone.kt
+++ src/oop_110866_Kiara/week06/Smartphone.kt
@@ -1,4 +1,10 @@
 package oop_110866_Kiara.week06
 
 class Smartphone: Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+
+        println("Smartphone Operation System is online")
+    }
 }
