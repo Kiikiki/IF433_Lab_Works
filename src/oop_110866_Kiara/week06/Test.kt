cf4dc927d86e8dbbf6b1d6e464137ec8f79c5779
Kiikiki <allysiakiara@gmail.com>
2026-03-09 15:34:05 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_110866_Kiara/week06/Test.kt
@@ -0,0 +1,8 @@
+package oop_110866_Kiara.week06
+
+fun main() {
+    val Lamp = SmartLamp("L01", "Guest Room")
+    val speaker = SmartLamp("S01", "Google Kitchen Nest")
+    val CCTV = SmartLamp("C01", "Ezviz Garage")
+
+}
