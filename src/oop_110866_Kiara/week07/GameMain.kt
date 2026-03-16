ad7e62a5c08e3af9da06a2226c6e1600ca3a2af3
Kiikiki <allysiakiara@gmail.com>
2026-03-16 19:46:41 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_110866_Kiara/week07/GameMain.kt
+++ src/oop_110866_Kiara/week07/GameMain.kt
@@ -1,8 +1,20 @@
 package oop_110866_Kiara.week07
 
 fun main() {
+    //start game
     GameManager.startGame()
     GameManager.startGame()
+
+    //check LEGENDARY drop chance
+    println("Legendary has a ${ItemRarity.LEGENDARY}% chance to drop")
+
+    //init sword
+    val sword1 = Weapon.forgeStarterSword()
+    println("=== Starter Sword ===")
+    println("name       : ${sword1.item.name}")
+    println("damage     : ${sword1.item.damage}")
+    println("rarity     : ${sword1.item.rarity.name}")
+    println("durability : ${sword1.durability}")
 
 
 }
