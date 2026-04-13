84963ac4e69e800a11557823173b6004bd09f967
Kiikiki <allysiakiara@gmail.com>
2026-04-13 16:33:24 +0700

    week08: (task) create sealed class Product and data classes

--- src/oop_110866_Kiara/week08/LegacyJavaAPI.java
+++ src/oop_110866_Kiara/week08/LegacyJavaAPI.java
@@ -2,6 +2,6 @@ package oop_110866_Kiara.week08;
 
 public class LegacyJavaAPI {
     public static String fetchServerStatus() {
-        return "SERVVER_ONLINE_200_OK";
+        return "SERVER_ONLINE_200_OK";
     }
 }
