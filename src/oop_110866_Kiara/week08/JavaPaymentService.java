5d47b837ae99df7b6aef8a10eecff888a6620a91
Kiikiki <allysiakiara@gmail.com>
2026-04-13 16:49:47 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_110866_Kiara/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_110866_Kiara.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
+
