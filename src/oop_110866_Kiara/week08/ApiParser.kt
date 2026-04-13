a83bec770056b8ae81e8df1597c3e2fbd34af4c8
Kiikiki <allysiakiara@gmail.com>
2026-04-13 22:18:10 +0700

    week08: (task) process mixed raw JSON list using as?

--- src/oop_110866_Kiara/week08/ApiParser.kt
+++ src/oop_110866_Kiara/week08/ApiParser.kt
@@ -25,16 +25,18 @@ class ApiParser {
 
         return null
     }
-}
 
-fun checkout(product: Product) {
-    //extract id
-    val id = when (product) {
-        is Electronic -> product.id
-        is Clothing -> product.id
+    fun checkout(product: Product) {
+        //extract id
+        val id = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+
+        val transactionId = JavaPaymentService.processPayment(id)!!
+        println(transactionId)
     }
-
-    val transactionId = JavaPaymentService.processPayment(id)!!
-    println(transactionId)
 }
+
+
 
