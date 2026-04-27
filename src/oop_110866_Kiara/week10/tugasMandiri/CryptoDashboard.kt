bbc38ec2a1ae3ddafd4f3264950eda3fc865a6fc
Kiikiki <allysiakiara@gmail.com>
2026-04-27 13:24:40 +0700

    week10: (task) populate coin repository

--- src/oop_110866_Kiara/week10/tugasMandiri/CryptoDashboard.kt
+++ src/oop_110866_Kiara/week10/tugasMandiri/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_110866_Kiara.week10.tugasMandiri
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 100.0))
+    coinRepo.add(Coin("ETH", 200.0))
+    coinRepo.add(Coin("USDT", 300.0))
 }
