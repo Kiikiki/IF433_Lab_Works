a036147722f735c73780e8d0bb40ec6b771c02c8
Kiikiki <allysiakiara@gmail.com>
2026-04-27 13:20:35 +0700

    week10: (task) implement search logic in repository

--- src/oop_110866_Kiara/week10/tugasMandiri/WalletRepository.kt
+++ src/oop_110866_Kiara/week10/tugasMandiri/WalletRepository.kt
@@ -1,11 +1,23 @@
 package oop_110866_Kiara.week10.tugasMandiri
 
-class WalletRepository<T>(private val items: MutableList<T> = mutableListOf()) {
+class WalletRepository<T>{
+    private val items = mutableListOf<T>()
+
     fun add(item: T) {
         items.add(item)
     }
 
     fun getAll(): List<T> {
         return items
+    }
+
+    fun searchByName(name: String): List<T> {
+        return items.filter {
+            when (it) {
+                is Coin -> it.name.contains(name, ignoreCase = true)
+                is Transaction -> it.id.contains(name, ignoreCase = true)
+                else -> false
+            }
+        }
     }
 }
