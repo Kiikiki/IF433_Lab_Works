cac852507b9726ece31f0f5bc263b3926f86a45a
Kiikiki <allysiakiara@gmail.com>
2026-05-04 14:29:18 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_110866_Kiara/week11/Task/SmartHomeDashboard.kt
+++ src/oop_110866_Kiara/week11/Task/SmartHomeDashboard.kt
@@ -5,10 +5,19 @@ fun main() {
 
     SmartDevice().apply {
         name = "Philips WiZ Living Room"
-        category = "Lighting Room"
+        category = "Lighting"
         isOnline = true
         powerLoad = 12
     }.also {
         homeDevice.add(it)
+    }
+
+    SmartDevice().apply {
+        name = "Ezviz Outdoor"
+        category = "Camera"
+        powerLoad = 5
+    }.also {
+        homeDevice.add(it)
+        println("[LOG] Camera is Connected")
     }
 }
