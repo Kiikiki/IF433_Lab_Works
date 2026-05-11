6595b061e95d73397b85d6bb418ddf62518c36a6
Kiikiki <allysiakiara@gmail.com>
2026-05-11 15:03:26 +0700

    week12: implement try as expression

--- src/oop_110866_Kiara/week12/ArithmeticException.kt
+++ src/oop_110866_Kiara/week12/ArithmeticException.kt
@@ -12,3 +12,16 @@ fun divide(a: Int, b: Int): Int {
         println("Division finished")
     }
 }
+
+fun main() {
+    println("=== Test Try As Expression ===")
+    val inputString = "123A"
+
+    val result: Int = try {
+        Integer.parseInt(inputString)
+    }
+    catch (e: NumberFormatException) {
+        -1
+    }
+    println("Parsing Result: $result")
+}
