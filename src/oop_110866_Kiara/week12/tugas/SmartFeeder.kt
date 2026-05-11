fbb6b070ea2a88e79654051b25f9cb24113cc796
Kiikiki <allysiakiara@gmail.com>
2026-05-11 21:02:52 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_110866_Kiara/week12/tugas/SmartFeeder.kt
+++ src/oop_110866_Kiara/week12/tugas/SmartFeeder.kt
@@ -2,4 +2,8 @@ package oop_110866_Kiara.week12.tugas
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Requested Grams must be more than 0 gr." }
+
+    if (isJammed) {
+        throw FeederException.DispenserJamException()
+    }
 }
