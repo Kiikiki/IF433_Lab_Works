fac83b9f6d7832a15a0161f0d35fe054a8bc7d0f
Kiikiki <allysiakiara@gmail.com>
2026-05-11 20:55:50 +0700

    week12: (task) create custom feeder exception subclasses

--- src/oop_110866_Kiara/week12/tugas/FeederExceptions.kt
+++ src/oop_110866_Kiara/week12/tugas/FeederExceptions.kt
@@ -1,4 +1,7 @@
 package oop_110866_Kiara.week12.tugas
 
 sealed class FeederException(msg: String) : Exception(msg) {
+    class FoodEmptyException(requested: Int, available: Int) : FeederException("Kibble isn't enough! Request $requested gr, Available $available gr")
+    class DispenserJamException : FeederException("[Error] Dispenser got jammed!")
 }
+
