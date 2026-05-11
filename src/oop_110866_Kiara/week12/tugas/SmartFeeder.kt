10cb27fa9721d3e01a509938587ee993cbedd768
Kiikiki <allysiakiara@gmail.com>
2026-05-11 21:00:47 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_110866_Kiara/week12/tugas/SmartFeeder.kt
@@ -0,0 +1,5 @@
+package oop_110866_Kiara.week12.tugas
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Requested Grams must be more than 0 gr." }
+}
