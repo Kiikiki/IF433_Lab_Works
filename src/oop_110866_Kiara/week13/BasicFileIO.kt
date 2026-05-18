68d4b1d252d99179e5f6e58a19ede95453b1658d
Kiikiki <allysiakiara@gmail.com>
2026-05-18 21:00:33 +0700

    week13: implement appendText operation

--- src/oop_110866_Kiara/week13/BasicFileIO.kt
+++ src/oop_110866_Kiara/week13/BasicFileIO.kt
@@ -6,4 +6,7 @@ fun main() {
     val file = File("notes.txt")
     file.writeText("Line 1: System Initialization\n")
     println("File Successfully made and written")
+
+    file.appendText("Line 2: Add new configuration\n")
+    println("Text successfully appended")
 }
